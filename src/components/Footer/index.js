c41e3e37b6a1428e670a1033356b91f6ee4ad944
Lucas Mendonça <lucas21rio@gmail.com>
2021-11-16 23:00:37 -0300

    img footer consertada

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
           <div className="footer">
           <div id="contato">
            <a target="_blank" href="https://github.com/iaeluk"><img src="https://cdn1.iconfinder.com/data/icons/picons-social/57/github_rounded-512.png" /></a>
-           <a href="mailto:lucas21rio@gmail.com"><img id="email-icon" src="https://cdn-icons.flaticon.com/png/512/3178/premium/3178158.png?token=exp=1637111020~hmac=6056adc8fec8426d8c0ebc0d9f1f0d61" /></a>
+           <a href="mailto:lucas21rio@gmail.com"><img id="email-icon" src="https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_email-512.png" /></a>
           </div>
           </div>
       )
